2d5bafb9fc61b0f99f9cc5158fe20d73b05e3fe2
Susadi11 <susadisandanima@gmail.com>
2024-05-10 10:05:11 +0530

    added home page

--- app/src/main/java/com/example/taskmanagement/MainActivity.kt
+++ app/src/main/java/com/example/taskmanagement/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.taskmanagement
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -9,5 +11,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.splash_screen)
+
+        // Delayed navigation to HomeActivity after 1000 milliseconds (1 second)
+        Handler().postDelayed({
+            // Start HomeActivity
+            startActivity(Intent(this, HomeActivity::class.java))
+            // Finish MainActivity
+            finish()
+        }, 1000) // 1000 milliseconds = 1 second
     }
 }
